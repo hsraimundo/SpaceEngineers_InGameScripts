a39c334b9d9cf49e304c40838f81a2229d720297
hsraimundo <hugo@hsraimundo.com>
2016-01-17 19:29:55 +0000

    Added battery script

--- SpaceEngineers_InGameScripts/IngameScripts/BatteryStatus.cs
+++ SpaceEngineers_InGameScripts/IngameScripts/BatteryStatus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sandbox.ModAPI.Ingame;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,50 @@ namespace SpaceEngineersScripts.InGame
     {
         public override void Main(string argument)
         {
-            throw new NotImplementedException();
+            //get all batteries
+            List<IMyTerminalBlock> batteries = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batteries);
+
+            //get the lcd configured in the argument
+            var lcd = (IMyTextPanel)GridTerminalSystem.GetBlockWithName(argument);
+
+            //auxiliary
+            List<String> statuses = new List<string>();
+            double avgPower = 0;
+
+            for (int i = 0; i < batteries.Count; ++i)
+            {
+                var match = System.Text.RegularExpressions.Regex.Match(
+                    batteries[i].DetailedInfo, 
+                    @"Max Stored Power: (?<power>[0-9,.]*) (?<scale>[k,M]*)Wh(.|\W)*Stored power: (?<power2>[0-9,.]*) (?<scale2>[k,M]*)Wh"
+                    );
+                
+                var maxStoredPower = match.Groups["power"].Value;
+                var maxStoredPowerMultiplier = (match.Groups["scale"].Value == "M") ? 1000000 : (match.Groups["scale"].Value == "M") ? 1000 : 1;
+                var storedPower = match.Groups["power2"].Value;
+                var storedPowerMultiplier = (match.Groups["scale2"].Value == "M") ? 1000000 : (match.Groups["scale2"].Value == "M") ? 1000 : 1;
+
+                double d1 = 0, d2 = 0;
+                double.TryParse(storedPower, out d1);
+                double.TryParse(maxStoredPower, out d2);
+
+                var batteryPerc = (d1*storedPowerMultiplier) / (maxStoredPowerMultiplier * d2) * 100;
+                avgPower += batteryPerc;
+
+                statuses.Add(String.Format("{0}: {1}%", batteries[i].CustomName, batteryPerc));
+            }
+
+
+            avgPower = avgPower / statuses.Count;
+
+            statuses.Add("Avg. Power: " + avgPower + "%");
+            string echo = String.Join("\n", statuses);
+
+            lcd.WritePublicText(echo);
+            lcd.ShowTextureOnScreen();
+            lcd.ShowPublicTextOnScreen();
+
+            Echo(echo);
         }
     }
 }
---
+++ SpaceEngineers_InGameScripts/Program.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceEngineers_InGameScripts
+{
+    public static class Program
+    {
+        static public void Main(string[] args)
+        {
+            IMyBatteryBlock battery = new MyBatteryBlock();
+            //for the sake of compilation - useful for ad-hoc testing as well
+        }
+    }
+}
