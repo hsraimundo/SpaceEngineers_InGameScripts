d69ff727c60dff5202798896ce309a1235deebb7
hsraimundo <hugo@hsraimundo.com>
2016-01-17 15:30:52 +0000

    Create base wrapper class

---
+++ SpaceEngineers_InGameScripts/Program.cs
@@ -0,0 +1,124 @@
+﻿using Sandbox.ModAPI;
+using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceEngineers_InGameScripts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //main method for... nothing?
+        }
+    }
+
+    public abstract class ProgrammableBlockInterface : IMyGridProgram
+    {
+        #region IMygridProgram
+        /// <summary>
+        /// Writes to the detailed info pane
+        /// </summary>
+        public Action<string> Echo
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Gets the elapsed time since the last execution
+        /// </summary>
+        public TimeSpan ElapsedTime
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Access to the full grid terminal system
+        /// </summary>
+        public IMyGridTerminalSystem GridTerminalSystem
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Auxiliary function. Inner works only.
+        /// </summary>
+        public bool HasMainMethod
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// The current programmable block
+        /// </summary>
+        public IMyProgrammableBlock Me
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Storage string. This is persisted between saves.
+        /// </summary>
+        public string Storage
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #endregion
+
+        abstract public void Main(string argument);
+    }
+
+    public class BatteryStatusScript : ProgrammableBlockInterface
+    {
+        public override void Main(string argument)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
